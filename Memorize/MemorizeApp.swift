8019b6ce73a2896d7757f4aaafd1417de6f180d4
Cleyson Silva <cdsilva1@topaz.com.br>
2021-12-21 20:33:49 -0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Cleyson Silva on 21/12/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
